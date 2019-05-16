3e531249070f231cccbaeff73ca44a8d0729ec06
AmritSidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2019-05-16 14:06:07 +0100

    Fix more linting issues
    Linting fixes from latest branch

--- app/controllers/assessment_questions_controller.rb
+++ app/controllers/assessment_questions_controller.rb
@@ -11,7 +11,7 @@ class AssessmentQuestionsController < AssessmentsController
 
     end
 
-    get_assessment()['confidence_level_required'] = params[:confidence_level_required]
+    get_assessment['confidence_level_required'] = params[:confidence_level_required]
 
     redirect_to controller: 'assessments', action: 'overview'
   end
@@ -21,15 +21,15 @@ class AssessmentQuestionsController < AssessmentsController
   end
 
   def choose_evidence_post
-    if (not params[:evidence_type]) && (params[:evidence_type_other].blank?)
+    if (not params[:evidence_type]) && params[:evidence_type_other].blank?
       @errors[:evidence_type] = 'You must choose a piece of evidence'
-      render "assessments/choose-evidence" and return
+      render("assessments/choose-evidence") && return
     end
-    if params[:id] == "new"
-      id = SecureRandom.uuid
-    else
-      id = params[:id]
-    end
+    id = if params[:id] == "new"
+           SecureRandom.uuid
+         else
+           params[:id]
+         end
     if params[:choose_evidence] == "other"
       params[:evidence_type] = nil
     else
@@ -40,10 +40,9 @@ class AssessmentQuestionsController < AssessmentsController
     redirect_to controller: 'assessments', action: 'overview'
   end
 
-  private
+private
 
   def get_evidence(id)
-    return ((get_assessment()['evidence'] ||= Hash.new)[id] ||= Hash.new)
+    ((get_assessment['evidence'] ||= Hash.new)[id] ||= Hash.new)
   end
-
 end
