e467982db7b3e28554078da21ecb16f0de575813
George Lund <george.lund@digital.cabinet-office.gov.uk>
2019-06-07 20:13:54 +0100

    Add comments giving pointers to explain our FormResponses base class

--- app/models/form_responses.rb
+++ app/models/form_responses.rb
@@ -1,6 +1,14 @@
+# Base class for (non-ActiveRecord) models providing serialization and attribute access
 class FormResponses
   include ActiveModel::Model
   include ActiveModel::Serialization
+
+  # Derived classes should provide ATTRS (a list of field name symbols), this is
+  # necessary for our serialization process (see #attributes).
+  #
+  # Background https://guides.rubyonrails.org/active_model_basics.html
+  #            https://api.rubyonrails.org/classes/ActiveModel/Model.html
+  #            https://api.rubyonrails.org/classes/ActiveModel/Serialization.html
 
   ATTRS = [:id].freeze
   attr_accessor(*ATTRS)
@@ -17,4 +25,6 @@ class FormResponses
     self.class::ATTRS.each { |attribute| all[attribute] = nil }
     all
   end
+
+  # attributes= is handled for us by ActiveModel::Model
 end
