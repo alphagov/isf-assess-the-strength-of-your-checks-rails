3539eb465f849184716763cf2d4580f4f0b73e7e
George Lund <george.lund@digital.cabinet-office.gov.uk>
2019-06-26 13:52:30 +0100

    Add long-version evidence flow that hits every question/page.
     - not quite every code path is tested yet, we'd look at doing
       that once we have some scoring.
    
    https://trello.com/c/WvYKd5qn/153-additional-evidence-questions-in-tool

--- spec/system/add_evidence_flow_spec.rb
+++ spec/system/add_evidence_flow_spec.rb
@@ -41,6 +41,15 @@ RSpec.feature 'Add evidence flow', type: :system do
     then_i_get_a_score('XX', 'out of 4')
     and_i_can_see_that_evidence_in_the_overview('UK passport')
   end
+
+  scenario 'Add new evidence to an assessment (long version – yes to all questions)' do
+    when_i_start_a_new_assessment
+    and_i_choose_a_regular_confidence_level
+    and_i_add_a_new_piece_of_evidence('Passport or travel document', 'UK passport')
+    and_i_answer_yes_to_every_question
+    then_i_get_a_score('XX', 'out of 4')
+    and_i_can_see_that_evidence_in_the_overview('UK passport')
+  end
 end
 
 def and_i_add_a_new_piece_of_evidence(evidence_group, evidence_type)
@@ -61,6 +70,81 @@ def and_i_answer_no_to_every_question
   ['physical features', 'cryptographic security', 'authoritative source', 'cancelled'].each do |question_snippet|
     expect(page).to have_content question_snippet
     choose 'No'
+    click_button 'Continue'
+  end
+end
+
+def and_i_answer_yes_to_every_question
+  ['physical features', 'original', 'errors', 'document capture app', 'recognised guidance', 'taught how to tell'].each do |question_snippet|
+    expect(page).to have_content question_snippet
+    choose 'Yes'
+    click_button 'Continue'
+  end
+
+  expect(page).to have_content 'refresh their training'
+  choose 'Every year'
+  click_button 'Continue'
+
+  expect(page).to have_content 'official templates'
+  choose 'Yes'
+  click_button 'Continue'
+
+  expect(page).to have_content 'templates updated'
+  choose 'Every year'
+  click_button 'Continue'
+
+  ['expired', 'intercepted', 'visible security features', 'protects them from being tampered with'].each do |question_snippet|
+    expect(page).to have_content question_snippet
+    choose 'Yes'
+    click_button 'Continue'
+  end
+
+  expect(page).to have_content 'security features'
+  check 'Designs printed using intaglio (raised) ink'
+  check 'Designs that have been laser etched'
+  check 'Watermarks'
+  check 'Security fibres'
+  check "Secondary background ('ghost') images"
+  click_button 'Continue'
+
+  expect(page).to have_content 'consistent throughout'
+  choose 'Yes'
+  click_button 'Continue'
+
+  expect(page).to have_content 'equipment'
+  check 'magnification tool'
+  check 'low angle'
+  check 'Another piece of equipment'
+  check 'A non-specialist light source'
+  click_button 'Continue'
+
+  ['controlled', 'supervised', 'ultraviolet (UV) or infrared (IR)'].each do |question_snippet|
+    expect(page).to have_content question_snippet
+    choose 'Yes'
+    click_button 'Continue'
+  end
+
+  expect(page).to have_content 'UV or IR security features'
+  check 'The paper the document is printed on'
+  check 'The layout and design of the document'
+  check 'Any fluorescent features'
+  click_button 'Continue'
+
+  expect(page).to have_content 'cryptographic security'
+  choose 'Yes'
+  click_button 'Continue'
+
+  expect(page).to have_content 'cryptographic security features'
+  check 'Check the evidence has not expired'
+  check 'Check the digital signature is correct'
+  check 'Check the signing key belongs to the organisation that issued the evidence'
+  check 'Check the signing key is correct for that type of evidence'
+  check 'Check the signing key has not been revoked'
+  click_button 'Continue'
+
+  ['authoritative source', 'cancelled'].each do |question_snippet|
+    expect(page).to have_content question_snippet
+    choose 'Yes'
     click_button 'Continue'
   end
 end
